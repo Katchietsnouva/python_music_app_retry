5736bdbd52b611039d870aab096c46a47748cd96
Katchietsnouva <philipaswa01@gmail.com>
2023-09-16 00:44:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -39,9 +39,9 @@ list_of_covers = [
 ]
 
 def get_album_cover(song_name,n):
-    image1 = Image.open(list_of_covers[n])
+    image1 = PIL.Image.open(list_of_covers[n])
     image2=image1.resize((250, 250))
-    load = ImageTk.PhotoImage(image2)
+    load = PIL.ImageTk.PhotoImage(image2)
     
     label1 = customtkinter.Label(root, image=load)
     label1.image = load
@@ -70,16 +70,17 @@ def play_music():
     global n
     
     current_song = n
-    if n>2:
-        n=0
+    if n > 2:
+        n = 0
     song_name = list_of_songs[n]
     pygame.mixer.music.load(song_name)
     pygame.mixer.music.play(loops=0)
-    pygame.music.mixer.set_volume(.5)
-    get_album_cover(song_name,n)
+    pygame.mixer.set_volume(0.5)  # Adjust the volume here
+    get_album_cover(song_name, n)
 
     print('PLAY')
-    n+=1
+    n += 1
+
 
 def pause_music():
     pass
