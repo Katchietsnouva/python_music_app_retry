d867e8e28907f0260e960b8d546e4eefb4f55b5c
Katchietsnouva <philipaswa01@gmail.com>
2023-09-15 22:54:15 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,12 +8,7 @@ import time
 import math
 
 
-# importing modules
-from tkinter import *
-import customtkinter
-import pygame
-from threading import *
-
+# Set customtkinter appearance mode and color theme
 customtkinter.set_appearance_mode("System")
 customtkinter.set_default_color_theme("blue")
 
@@ -23,13 +18,13 @@ root.geometry('400x480')
 pygame.mixer.init()
 
 list_of_songs = [
-    'nouvamusic\Feelings_7.1_3.1.mp3',
-    'nouvamusic\Hard Trap Variant.mp3',
-    'nouvamusic\Playful EDM_2.mp3',
-    'nouvamusic\Serum_6.mp3',
-    'nouvamusic\Sing song_6.mp3',
-    'nouvamusic\Vacation_6.mp3',
-    'nouvamusic\You dont recognise my face_4.mp3'
+    'nouvamusic/Feelings_7.1_3.1.mp3',
+    'nouvamusic/Hard Trap Variant.mp3',
+    'nouvamusic/Playful EDM_2.mp3',
+    'nouvamusic/Serum_6.mp3',
+    'nouvamusic/Sing song_6.mp3',
+    'nouvamusic/Vacation_6.mp3',
+    'nouvamusic/You dont recognise my face_4.mp3'
 ]
 
 list_of_covers = []
@@ -49,19 +44,14 @@ def skip_b():
 def volume(value):
     print(value)
 
+    
 def adjust_button_positions(event):
     # Calculate the center of the window's width
     window_width = event.width
     button_x = window_width / 2
+    print(window_width)
+    print(button_x)
 
-    # Update button positions with the anchor set to CENTER
-    play_button.place(x=button_x, y=336, anchor='center')
-    skip_f_button.place(x=button_x + 80, y=336, anchor='center')
-    skip_b_button.place(x=button_x - 80, y=336, anchor='center')
-    slider.place(x=button_x, y=385, anchor='center')
-
-# Bind the function to the window resize event
-root.bind('<Configure>', adjust_button_positions)
 
 play_button = customtkinter.CTkButton(master=root, text='Play', command=play_music)
 skip_f_button = customtkinter.CTkButton(master=root, text='>', command=skip_f, width=2)
@@ -74,3 +64,6 @@ skip_b_button.place(x=120, y=336, anchor='center')
 slider.place(x=200, y=385, anchor='center')
 
 root.mainloop()
+
+
+
