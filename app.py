7c207e520046068828f0b9c69fe58d92fdd324e0
Katchietsnouva <philipaswa01@gmail.com>
2023-09-15 17:10:54 +0300

    Update app.py

--- app.py
+++ app.py
@@ -15,5 +15,6 @@ root = customtkinter.CTk()  # create CTk window like you do with the Tk window
 print('hi')
 root.title('Nouva Music')
 root.geometry('400x480')
+pygame.mixer.init()
 
 root.mainloop()
