fa9e6294a7f0f4a1a6692e8deedc901eb8b910df
Katchietsnouva <philipaswa01@gmail.com>
2023-09-15 17:01:32 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 # importing modules
 from tkinter import *
-import customtinkter
+import customtkinter
 import pygame
-from PIL import Image, ImageTk
+# from PIL import Image, ImageTk
 from threading import *
 import time
 import math
 
+customtkinter.set_appearance_mode("System")  # Modes: system (default), light, dark
+customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
 
+root = customtkinter.CTk()  # create CTk window like you do with the Tk window
+
+print('hi')
+
+root.mainloop()
