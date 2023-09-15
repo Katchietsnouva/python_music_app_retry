2b3832aa26398804a1bf44a7bf17d0ec41775755
Katchietsnouva <philipaswa01@gmail.com>
2023-09-15 17:04:22 +0300

    Update app.py

--- app.py
+++ app.py
@@ -13,5 +13,7 @@ customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-bl
 root = customtkinter.CTk()  # create CTk window like you do with the Tk window
 
 print('hi')
+root.title('Nouva Music')
+root.geometry('400x480')
 
 root.mainloop()
