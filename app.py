9ebec29786e7aad1767c0fa73163d337bb2b4c82
Katchietsnouva <philipaswa01@gmail.com>
2023-09-16 01:20:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ def threading():
     t1.start()
 
 def play_music():
-    # threading()
+    threading()
     global n
     
     current_song = n
@@ -83,10 +83,10 @@ def play_music():
 
     
     # Update the progress bar while the music is playing
-    while pygame.mixer.music.get_busy():
-        progress = pygame.mixer.music.get_pos() / 1000  # Get progress in milliseconds
-        progressbar.set(progress / (a.get_length() * 3))  # Update the progress bar
-        time.sleep(0.1)  # Sleep for a short interval to avoid busy-waiting
+    # while pygame.mixer.music.get_busy():
+    #     progress = pygame.mixer.music.get_pos() / 1000  # Get progress in milliseconds
+    #     progressbar.set(progress / (a.get_length() * 3))  # Update the progress bar
+    #     time.sleep(0.1)  # Sleep for a short interval to avoid busy-waiting
 
 
 
