056ff0e797e449ab37d0a4a47f8dcb5762bcc7fb
Katchietsnouva <philipaswa01@gmail.com>
2023-09-15 23:00:36 +0300

    Update app2.py

--- app2.py
+++ app2.py
@@ -7,13 +7,21 @@ def get_screen_width():
     return screen_width
 
 def display_gui(screen_width):
+    def on_resize(event):
+        new_width = event.width
+        label.config(text=f"Screen Width: {new_width}")
+        print(f"Altered Screen Width: {new_width}")
+
     root = tk.Tk()
     root.title("Screen Width Display")
+    
     label = tk.Label(root, text=f"Screen Width: {screen_width}")
     label.pack(padx=20, pady=20)
+
+    root.bind("<Configure>", on_resize)
     root.mainloop()
 
 if __name__ == "__main__":
     screen_width = get_screen_width()
-    print(f"Screen Width: {screen_width}")
+    print(f"Initial Screen Width: {screen_width}")
     display_gui(screen_width)
